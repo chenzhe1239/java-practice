d59d4317b055c9cab0267c98be430036a6d6723b
Janos Gyerik <janos@kronos>
2015-11-11 11:32:59 +0100

    implemented solution skeleton, with filter and concat

--- src/test/java/com/janosgyerik/practice/oj/leetcode/medium/PartitionList/PartitionListTest.java
+++ src/test/java/com/janosgyerik/practice/oj/leetcode/medium/PartitionList/PartitionListTest.java
@@ -1,4 +1,48 @@
 package com.janosgyerik.practice.oj.leetcode.medium.PartitionList;
 
+import com.janosgyerik.practice.oj.leetcode.common.ListNode;
+import com.janosgyerik.practice.oj.leetcode.common.ListNodeUtils;
+import org.junit.Test;
+
+import java.util.function.Predicate;
+
+import static org.junit.Assert.assertEquals;
+import static com.janosgyerik.practice.oj.leetcode.common.ListNodeUtils.create;
+
 public class PartitionListTest {
+    private final Solution solution = new Solution();
+
+    private String solve(int x, int ... values) {
+        ListNode head = create(values);
+        return ListNodeUtils.toString(solution.partition(head, x));
+    }
+
+    @Test
+    public void partition_1_2_1_by_2() {
+        assertEquals("1->1->2", solve(2, 1, 2, 1));
+    }
+
+    private String filter(Predicate<? super Integer> predicate, int ... values) {
+        return ListNodeUtils.toString(Solution.filter(create(values), predicate));
+    }
+
+    @Test
+    public void test_filter_1_2_3_by_gt1() {
+        assertEquals("2->3", filter(e -> e > 1, 1, 2, 3));
+    }
+
+    @Test
+    public void test_filter_1_2_3_by_gt2() {
+        assertEquals("3", filter(e -> e > 2, 1, 2, 3));
+    }
+
+    @Test
+    public void test_filter_1_2_3_by_gt0() {
+        assertEquals("1->2->3", filter(e -> e > 0, 1, 2, 3));
+    }
+
+    @Test
+    public void test_filter_1_2_3_by_gt5() {
+        assertEquals("", filter(e -> e > 5, 1, 2, 3));
+    }
 }
--- src/test/java/com/janosgyerik/practice/oj/leetcode/medium/PartitionList/Solution.java
+++ src/test/java/com/janosgyerik/practice/oj/leetcode/medium/PartitionList/Solution.java
@@ -2,8 +2,26 @@ package com.janosgyerik.practice.oj.leetcode.medium.PartitionList;
 
 import com.janosgyerik.practice.oj.leetcode.common.ListNode;
 
+import java.util.function.Predicate;
+
 public class Solution {
     public ListNode partition(ListNode head, int x) {
-        return head;
+        return concat(filter(head, e -> e < x), filter(head, e -> e >= x));
+    }
+
+    protected ListNode concat(ListNode head1, ListNode head2) {
+        return null;
+    }
+
+    protected static ListNode filter(ListNode head, Predicate<? super Integer> predicate) {
+        ListNode result = new ListNode(0);  // dummy
+        ListNode runner = result;
+        for (ListNode node = head; node != null; node = node.next) {
+            if (predicate.test(node.val)) {
+                runner.next = new ListNode(node.val);
+                runner = runner.next;
+            }
+        }
+        return result.next;
     }
 }
