3aaaec605541f7ba7d7376b1f173440b01b9049a
Janos Gyerik <janos@kronos>
2015-11-11 11:21:50 +0100

    made ListNode.next public to make it usable in solutions

--- src/test/java/com/janosgyerik/practice/oj/leetcode/common/ListNode.java
+++ src/test/java/com/janosgyerik/practice/oj/leetcode/common/ListNode.java
@@ -2,7 +2,7 @@ package com.janosgyerik.practice.oj.leetcode.common;
 
 public class ListNode {
     int val;
-    ListNode next;
+    public ListNode next;
 
     public ListNode(int x) {
         val = x;
