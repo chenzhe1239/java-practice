3abd735039332fedde0430daf48579bba447c0de
Janos Gyerik <janos@kronos>
2015-11-13 00:14:39 +0100

    simple recursive logic

--- src/test/java/com/janosgyerik/practice/oj/leetcode/medium/LongestIncreasingSubsequence/Solution.java
+++ src/test/java/com/janosgyerik/practice/oj/leetcode/medium/LongestIncreasingSubsequence/Solution.java
@@ -1,27 +1,21 @@
 package com.janosgyerik.practice.oj.leetcode.medium.LongestIncreasingSubsequence;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Solution {
 
     public int lengthOfLIS(int[] nums) {
-        return lengthOfLIS(nums, Integer.MIN_VALUE, 0, 0, new HashMap<>());
+        return lengthOfLIS(nums, Integer.MIN_VALUE, 0, 0);
     }
 
-    private int lengthOfLIS(int[] nums, int value, int pos, int count, Map<Integer, Integer> results) {
-        int result;
+    private int lengthOfLIS(int[] nums, int value, int pos, int count) {
         if (pos >= nums.length) {
-            result = count;
-        } else if (nums[pos] < value) {
-            result = lengthOfLIS(nums, value, pos + 1, count, results);
-        } else {
-            result = Math.max(
-                    lengthOfLIS(nums, value, pos + 1, count, results),
-                    lengthOfLIS(nums, nums[pos], pos + 1, count + 1, results)
-            );
+            return count;
         }
-        results.put(pos, result);
-        return result;
+        if (nums[pos] < value) {
+            return lengthOfLIS(nums, value, pos + 1, count);
+        }
+        return Math.max(
+                lengthOfLIS(nums, value, pos + 1, count),
+                lengthOfLIS(nums, nums[pos], pos + 1, count + 1)
+                );
     }
 }
