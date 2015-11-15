77e49e4f3e5c6ad2b45469e5f809047fe1d1babf
Janos Gyerik <janos@kronos>
2015-11-15 18:46:03 +0100

    simplified house robber 2

--- src/test/java/com/janosgyerik/practice/oj/leetcode/medium/HouseRobber2/Solution.java
+++ src/test/java/com/janosgyerik/practice/oj/leetcode/medium/HouseRobber2/Solution.java
@@ -4,36 +4,20 @@ public class Solution {
     public int rob(int[] nums) {
         int length = nums.length;
 
-        if (length == 0) {
-            return 0;
+        switch (length) {
+            case 0: return 0;
+            case 1: return nums[0];
+            case 2: return Math.max(nums[0], nums[1]);
         }
-        if (length == 1) {
-            return nums[0];
-        }
 
-        // choose first num, so don't count last
-        int backup = nums[length - 1];
-        nums[length - 1] = 0;
-        int alt1 = robHelper(nums);
-        nums[length - 1] = backup;
-
-        // don't count first num
-        nums[0] = 0;
-        int alt2 = robHelper(nums);
-
-        // don't count first two nums
-        nums[1] = 0;
-        int alt3 = robHelper(nums);
-
-        return Math.max(alt1, Math.max(alt2, alt3));
+        return Math.max(rob(nums, 0, length - 1), rob(nums, 1, length));
     }
 
-    private int robHelper(int[] nums) {
-        int length = nums.length;
-        int maxAtPrevPrev = nums[0];
-        int maxAtPrev = Math.max(nums[0], nums[1]);
+    private int rob(int[] nums, int start, int end) {
+        int maxAtPrevPrev = nums[start];
+        int maxAtPrev = Math.max(nums[start], nums[start + 1]);
 
-        for (int i = 2; i < length; ++i) {
+        for (int i = start + 2; i < end; ++i) {
             int current = nums[i];
             int max = Math.max(maxAtPrevPrev + current, maxAtPrev);
             maxAtPrevPrev = maxAtPrev;
