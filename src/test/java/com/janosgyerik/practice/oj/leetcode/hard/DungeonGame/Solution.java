9333693b506077b3e0e169697eea348ad9bba129
Janos Gyerik <janos@kronos>
2015-11-17 21:30:03 +0100

    dungeons solved

--- src/test/java/com/janosgyerik/practice/oj/leetcode/hard/DungeonGame/Solution.java
+++ src/test/java/com/janosgyerik/practice/oj/leetcode/hard/DungeonGame/Solution.java
@@ -2,56 +2,35 @@ package com.janosgyerik.practice.oj.leetcode.hard.DungeonGame;
 
 public class Solution {
     public int calculateMinimumHP(int[][] grid) {
-        int min = grid[0][0];
-        int[][] xmin = createGrid(grid);
-        int[][] hp = createGrid(grid);
-
-        for (int row = 0; row < grid.length; ++row) {
-            for (int col = 0; col < grid[row].length; ++col) {
-                int xminLeft = getLeft(xmin, row, col);
-                int xminTop = getTop(xmin, row, col);
-                int hpLeft = getLeft(hp, row, col);
-                int hpTop = getTop(hp, row, col);
-
-                if (xminLeft < xminTop || xminLeft == xminTop && hpTop > hpLeft) {
-                    xmin[row][col] = Math.min(xminTop, hpTop + grid[row][col]);
-                    hp[row][col] = hpTop + grid[row][col];
-                } else {
-                    xmin[row][col] = Math.min(xminLeft, hpLeft + grid[row][col]);
-                    hp[row][col] = hpLeft + grid[row][col];
-                }
-                min = xmin[row][col];
+        int min = 0;
+        for (int row = grid.length - 1; row >= 0 ;--row) {
+            for (int col = grid[row].length - 1; col >= 0; --col) {
+                int requiredForRight = getRight(grid, row, col);
+                int requiredForDown = getDown(grid, row, col);
+                min = Math.min(0, grid[row][col] + Math.max(requiredForDown, requiredForRight));
+                grid[row][col] = min;
             }
         }
         return 1 - Math.min(0, min);
     }
 
-    private int[][] createGrid(int[][] grid) {
-        int[][] newGrid = new int[grid.length][];
-        int width = grid[0].length;
-        for (int i = 0; i < newGrid.length; ++i) {
-            newGrid[i] = new int[width];
-        }
-        return newGrid;
-    }
-
-    private int getLeft(int[][] grid, int row, int col) {
-        if (col == 0) {
-            if (row == 0) {
+    private int getRight(int[][] grid, int row, int col) {
+        if (col == grid[0].length - 1) {
+            if (row == grid.length - 1) {
                 return 0;
             }
-            return grid[row - 1][col];
+            return grid[row + 1][col];
         }
-        return grid[row][col - 1];
+        return grid[row][col + 1];
     }
 
-    private int getTop(int[][] grid, int row, int col) {
-        if (row == 0) {
-            if (col == 0) {
+    private int getDown(int[][] grid, int row, int col) {
+        if (row == grid.length - 1) {
+            if (col == grid[0].length - 1) {
                 return 0;
             }
-            return grid[row][col - 1];
+            return grid[row][col + 1];
         }
-        return grid[row - 1][col];
+        return grid[row + 1][col];
     }
 }
