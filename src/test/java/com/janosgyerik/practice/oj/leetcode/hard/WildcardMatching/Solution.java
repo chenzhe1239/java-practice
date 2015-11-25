0d49b966bcd55a938dfbbd8f506d5059de8272cd
Janos Gyerik <janos@kronos>
2015-11-25 20:33:15 +0100

    added memoization

--- src/test/java/com/janosgyerik/practice/oj/leetcode/hard/WildcardMatching/Solution.java
+++ src/test/java/com/janosgyerik/practice/oj/leetcode/hard/WildcardMatching/Solution.java
@@ -1,15 +1,18 @@
 package com.janosgyerik.practice.oj.leetcode.hard.WildcardMatching;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
     public boolean isMatch(String text, String pattern) {
-        return isMatchHelper(text, simplify(pattern));
+        return isMatchHelper(text, simplify(pattern), new HashSet<>());
     }
 
     private String simplify(String pattern) {
         return pattern.replaceAll("\\*{2,}", "*");
     }
 
-    public boolean isMatchHelper(String text, String pattern) {
+    public boolean isMatchHelper(String text, String pattern, Set<String> unmatched) {
         int iText = 0;
         for (int iPattern = 0; iText < text.length() && iPattern < pattern.length(); ++iText, ++iPattern) {
             char cText = text.charAt(iText);
@@ -25,10 +28,15 @@ public class Solution {
             } else {
                 String subPattern = pattern.substring(iPattern + 1);
                 for (; iText < text.length(); ++iText) {
-                    boolean match = isMatchHelper(text.substring(iText), subPattern);
+                    String subText = text.substring(iText);
+                    if (unmatched.contains(subText)) {
+                        continue;
+                    }
+                    boolean match = isMatchHelper(subText, subPattern, new HashSet<>());
                     if (match) {
                         return true;
                     }
+                    unmatched.add(subText);
                 }
                 return false;
             }
