bae053555da5c9624407cda6b134471165b991b5
Janos Gyerik <janos@kronos>
2015-11-25 20:20:26 +0100

    moved iPattern inside

--- src/test/java/com/janosgyerik/practice/oj/leetcode/hard/WildcardMatching/Solution.java
+++ src/test/java/com/janosgyerik/practice/oj/leetcode/hard/WildcardMatching/Solution.java
@@ -3,19 +3,15 @@ package com.janosgyerik.practice.oj.leetcode.hard.WildcardMatching;
 public class Solution {
     public boolean isMatch(String text, String pattern) {
         int iText = 0;
-        int iPattern = 0;
-        while (iText < text.length() && iPattern < pattern.length()) {
+        for (int iPattern = 0; iText < text.length() && iPattern < pattern.length(); ++iText, ++iPattern) {
             char cText = text.charAt(iText);
             char cPattern = pattern.charAt(iPattern);
             if (cPattern == '?') {
-                ++iText;
-                ++iPattern;
+                // nothing to do
             } else if (cPattern != '*') {
                 if (cText != cPattern) {
                     return false;
                 }
-                ++iText;
-                ++iPattern;
             } else if (iPattern == pattern.length() - 1) {
                 return true;
             } else {
