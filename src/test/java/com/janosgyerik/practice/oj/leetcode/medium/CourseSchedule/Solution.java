0b8aef2c2e335d45fe73924a12ecc7fc3c53be21
Janos Gyerik <janos@kronos>
2016-01-07 18:24:17 +0100

    alternative, more elegant solution

--- src/test/java/com/janosgyerik/practice/oj/leetcode/medium/CourseSchedule/Solution.java
+++ src/test/java/com/janosgyerik/practice/oj/leetcode/medium/CourseSchedule/Solution.java
@@ -3,54 +3,67 @@ package com.janosgyerik.practice.oj.leetcode.medium.CourseSchedule;
 import java.util.*;
 
 public class Solution {
+
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        Map<Integer, Set<Integer>> map = toMap(prerequisites);
-
-        boolean[] visited = new boolean[numCourses];
-        boolean[] finishable = new boolean[numCourses];
-
-        for (int index : map.keySet()) {
-            if (!canFinish(map, visited, index, finishable)) {
-                return false;
-            } else {
-                finishable[index] = true;
-            }
-        }
-        return true;
+        Graph graph = createGraph(numCourses, prerequisites);
+        return canFinish(graph);
     }
 
-    private boolean canFinish(Map<Integer, Set<Integer>> map, boolean[] visited, int index, boolean[] finishable) {
-        if (finishable[index]) {
-            return true;
-        }
-        if (visited[index]) {
-            return false;
-        }
-        visited[index] = true;
-        Set<Integer> prerequisites = map.get(index);
-        if (prerequisites == null) {
-            return true;
-        }
-        for (int req : prerequisites) {
-            if (!canFinish(map, visited, req, finishable)) {
-                return false;
-            } else {
-                finishable[req] = true;
-            }
-        }
-        return true;
+    private boolean canFinish(Graph graph) {
+        while (graph.trim());
+        return graph.isEmpty();
     }
 
-    private Map<Integer, Set<Integer>> toMap(int[][] prerequisites) {
-        Map<Integer, Set<Integer>> map = new HashMap<>();
-        for (int[] pair : prerequisites) {
-            Set<Integer> neighbors = map.get(pair[0]);
-            if (neighbors == null) {
-                neighbors = new HashSet<>();
-                map.put(pair[0], neighbors);
-            }
-            neighbors.add(pair[1]);
+    private Graph createGraph(int num, int[][] edges) {
+        Graph graph = new Graph(num);
+        for (int[] edge : edges) {
+            graph.addEdge(edge[0], edge[1]);
         }
-        return map;
+        return graph;
+    }
+
+    private static class Graph {
+        Map<Integer, Set<Integer>> incoming = new HashMap<>();
+        Map<Integer, Set<Integer>> outgoing = new HashMap<>();
+
+        public Graph(int num) {
+            for (int i = 0; i < num; ++i) {
+                incoming.put(i, new HashSet<>());
+                outgoing.put(i, new HashSet<>());
+            }
+        }
+
+        public void addEdge(int from, int to) {
+            outgoing.get(from).add(to);
+            incoming.get(to).add(from);
+        }
+
+        public boolean trim() {
+            boolean anyRemoved = false;
+            for (Map.Entry<Integer, Set<Integer>> entry : new ArrayList<>(incoming.entrySet())) {
+                if (entry.getValue().isEmpty()) {
+                    remove(entry.getKey());
+                    anyRemoved = true;
+                }
+            }
+            return anyRemoved;
+        }
+
+        public void remove(Integer node) {
+            if (outgoing.containsKey(node)) {
+                for (Integer neighbor : outgoing.get(node)) {
+                    incoming.get(neighbor).remove(node);
+                    if (incoming.get(neighbor).isEmpty()) {
+                        remove(neighbor);
+                    }
+                }
+                outgoing.remove(node);
+            }
+            incoming.remove(node);
+        }
+
+        public boolean isEmpty() {
+            return incoming.isEmpty();
+        }
     }
 }
