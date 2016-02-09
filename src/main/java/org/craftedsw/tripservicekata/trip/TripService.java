1f3b3e8864b72f367e39fe1688ef42eb654d619a
Janos Gyerik <janos@kronos>
2016-02-09 20:46:38 +0100

    rewrote without mocks

--- src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -1,40 +1,17 @@
 package org.craftedsw.tripservicekata.trip;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TripService {
 
-	private final UserSession userSession;
-	private final TripDAO tripDAO;
-
-	static class TripDAOWrapper extends TripDAO {
-		@Override
-		public List<Trip> findTripsByUser(User user) {
-			return TripDAO.findTripsByUser_deprecated(user);
-		}
-	}
-
-	public TripService() {
-		this(UserSession.getInstance());
-	}
-
-	public TripService(UserSession userSession) {
-		this(userSession, new TripDAOWrapper());
-	}
-
-	public TripService(UserSession userSession, TripDAO tripDAO) {
-		this.userSession = userSession;
-		this.tripDAO = tripDAO;
-	}
-
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = userSession.getLoggedUser();
+		List<Trip> tripList = new ArrayList<>();
+		User loggedUser = getLoggedUser();
 		boolean isFriend = false;
 		if (loggedUser != null) {
 			for (User friend : user.getFriends()) {
@@ -44,12 +21,20 @@ public class TripService {
 				}
 			}
 			if (isFriend) {
-				tripList = tripDAO.findTripsByUser(user);
+				tripList = findTripsByUser(user);
 			}
 			return tripList;
 		} else {
 			throw new UserNotLoggedInException();
 		}
 	}
-	
+
+	protected User getLoggedUser() {
+		return UserSession.getInstance().getLoggedUser();
+	}
+
+	protected List<Trip> findTripsByUser(User user) {
+		return TripDAO.findTripsByUser(user);
+	}
+
 }
--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -2,50 +2,49 @@ package org.craftedsw.tripservicekata.trip;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
-import org.craftedsw.tripservicekata.user.UserSession;
 import org.junit.Test;
 
 import java.util.Collections;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class TripServiceTest {
 
-    private TripService createTripService(UserSession userSession) {
-        return new TripService(userSession);
+    private TripService createTripService(User user) {
+        return createTripService(user, null);
     }
 
-    private TripService createTripService(UserSession userSession, TripDAO tripDAO) {
-        return new TripService(userSession, tripDAO);
+    private TripService createTripService(User user, List<Trip> trips) {
+        return new TripService() {
+            @Override
+            protected User getLoggedUser() {
+                return user;
+            }
+
+            @Override
+            protected List<Trip> findTripsByUser(User user) {
+                return trips;
+            }
+        };
     }
 
     @Test(expected = UserNotLoggedInException.class)
     public void test_getTripsByUser_should_throw_if_user_not_logged() {
-        TripService tripService = createTripService(mock(UserSession.class));
+        TripService tripService = createTripService(null);
         tripService.getTripsByUser(null);
     }
 
     @Test
     public void test_getTripsByUser_should_find_nothing_if_user_has_no_friends() {
-        User loggedUser = new User();
-
-        UserSession mockUserSession = mock(UserSession.class);
-        when(mockUserSession.getLoggedUser()).thenReturn(loggedUser);
-
-        TripService tripService = createTripService(mockUserSession);
+        TripService tripService = createTripService(new User());
         assertEquals(Collections.emptyList(), tripService.getTripsByUser(new User()));
     }
 
     @Test
-    public void test_getTripsByUser_should_find_nothing_if_loggedUser_not_a_friend() {
-        User loggedUser = new User();
+    public void test_getTripsByUser_should_find_nothing_if_loggedUser_is_not_a_friend() {
+        TripService tripService = createTripService(new User());
 
-        UserSession mockUserSession = mock(UserSession.class);
-        when(mockUserSession.getLoggedUser()).thenReturn(loggedUser);
-
-        TripService tripService = createTripService(mockUserSession);
         User user = new User();
         user.addFriend(new User());
         assertEquals(Collections.emptyList(), tripService.getTripsByUser(user));
@@ -54,12 +53,8 @@ public class TripServiceTest {
     @Test
     public void test_getTripsByUser_should_find_trips_if_loggedUser_is_a_friend() {
         User loggedUser = new User();
+        TripService tripService = createTripService(loggedUser, Collections.emptyList());
 
-        UserSession mockUserSession = mock(UserSession.class);
-        when(mockUserSession.getLoggedUser()).thenReturn(loggedUser);
-
-        TripDAO mockTripDAO = mock(TripDAO.class);
-        TripService tripService = createTripService(mockUserSession, mockTripDAO);
         User user = new User();
         user.addFriend(loggedUser);
         assertEquals(user.trips(), tripService.getTripsByUser(user));
