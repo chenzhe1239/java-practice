5b39f14f67206d65e1423f7a207c3ecd78565bc1
Janos Gyerik <janos@kronos>
2016-02-09 20:48:22 +0100

    simplified

--- src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -4,33 +4,27 @@ import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TripService {
 
     public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-        List<Trip> tripList = new ArrayList<>();
         User loggedUser = getLoggedUser();
-        boolean isFriend = false;
         if (loggedUser == null) {
             throw new UserNotLoggedInException();
         }
+
         for (User friend : user.getFriends()) {
             if (friend.equals(loggedUser)) {
-                isFriend = true;
-                break;
+                return findTripsByUser(user);
             }
         }
-        if (isFriend) {
-            tripList = findTripsByUser(user);
-        }
-        return tripList;
+        return Collections.emptyList();
     }
 
     protected User getLoggedUser() {
-        return UserSession.getInstance()
-                .getLoggedUser();
+        return UserSession.getInstance().getLoggedUser();
     }
 
     protected List<Trip> findTripsByUser(User user) {
