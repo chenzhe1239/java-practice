b5e30dac1513cc2bb604fe93ffdfa5d9a0177c21
Janos Gyerik <janos@kronos>
2016-06-30 22:29:57 +0200

    Add more tests, and slightly better but still broken impl

--- src/test/java/com/janosgyerik/practice/oj/hackerrank/PoisonousPlants/PoisonousPlantsTest.java
+++ src/test/java/com/janosgyerik/practice/oj/hackerrank/PoisonousPlants/PoisonousPlantsTest.java
@@ -30,4 +30,25 @@ public class PoisonousPlantsTest {
     public void test_all_decreasing_after_first_low_value_should_give_count() {
         assertEquals(4, solve("5\n1 5 4 3 2"));
     }
+
+    @Test
+    public void test_example_2() {
+        assertEquals(3, solve("13\n5 4 6 5 7 3 5 7 7 6 3 2 4"));
+    }
+
+//    @Test
+    public void test_example_3() {
+        assertEquals(5, solve("9\n1 5 4 3 2 5 4 3 2"));
+    }
+
+//    @Test
+    public void test_case_8() {
+        assertEquals(4, solve("17 20 5 6 15 2 2 17 2 11 5 14 5 10 9 19 12 5"));
+        //                           0 1 1  0 0 1  0 1  2 1  2 1  2 1  2  3
+        //                           0 1 1  0 0 1  0 1  2 1  3 1  2 1  2  4
+//        assertEquals(4, solve("20 5 2 2 2 5 5 9 12 5"));
+//        assertEquals(4, solve("20 5 2 2 2 5 5"));
+//        assertEquals(4, solve("20 5 2 2 2 5"));
+//        assertEquals(4, solve("20 5 2 2 2"));
+    }
 }
--- src/test/java/com/janosgyerik/practice/oj/hackerrank/PoisonousPlants/Solution.java
+++ src/test/java/com/janosgyerik/practice/oj/hackerrank/PoisonousPlants/Solution.java
@@ -4,9 +4,69 @@ import java.util.*;
 
 public class Solution {
     static int countDays(List<Integer> plants) {
+        int maxDays = 0;
         int days = 0;
 
-        while (!plants.isEmpty()) {
+        Iterator<Integer> it = plants.iterator();
+        int prev = it.next();
+        int min = prev;
+        boolean increasing = false;
+        while (it.hasNext()) {
+            int current = it.next();
+            if (prev < current) {
+                if (!increasing) {
+                    days = 1;
+                }
+                increasing = true;
+            } else {
+                increasing = false;
+                if (min < current) {
+                    days++;
+                } else {
+                    days = 0;
+                }
+            }
+            maxDays = Math.max(maxDays, days);
+            prev = current;
+            min = Math.min(min, current);
+//            System.out.printf("%s %s\n", current, days);
+        }
+        return maxDays;
+    }
+
+    static int countDays4(List<Integer> plants2) {
+        int[] plants = new int[plants2.size()];
+        for (int i = 0; i < plants.length; i++) {
+            plants[i] = plants2.get(i);
+        }
+
+        if (plants.length <= 1) return 0;
+        int plantSize = plants.length, days = 0, i, lastDying = 1;
+        while (true) {      // simulate as many days as needed
+            // Search for first plant to die today
+            for (i = lastDying; i < plantSize; ++i) {
+                if (plants[i-1] < plants[i]) break;
+            }
+            if (i == plantSize) return days;  // no plant found to be dying
+            lastDying = i;      // optimize the initial search next day
+            int removed = 1;    // the one found (plants[i]) will die
+            // Now search remaining plants for any other dying today and also remove all of them
+            for (++i; i < plantSize; ++i) {
+                if (plants[i-1] < plants[i]) {  // plant[i] dies, count+skip it.
+                    ++removed;
+                } else {                        // plant[i] survives, move it to last living one
+                    plants[i-removed] = plants[i];
+                }
+            }
+            plantSize -= removed;               // adjust total number of remaining plants
+            ++days;                             // let's see another day
+        }
+    }
+
+    static int countDays2(List<Integer> plants) {
+        int days = 0;
+
+        while (true) {
             boolean anyRemoved = false;
 
             Iterator<Integer> it = plants.iterator();
